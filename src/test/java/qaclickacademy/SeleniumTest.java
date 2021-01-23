cc775e3acbc175126e0f4e7453a096469698984c
VusiAppium <Vusi.Ngwenya@absa.africa>
2021-01-23 10:27:51 +0200

    Third commit

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -6,8 +6,10 @@ public class SeleniumTest
 {
     @Test
     public void browserAUtomation()
+
     {
       System.out.println("BrowserAutomation");
+        System.out.println("WebAutomation");
     }
 
     @Test
